9e26eed699f723bc6f4b9208dfef6132f1282c7d
VishwaDinath <vishwadinath97@gmail.com>
2023-09-16 17:08:26 +0530

    code challenge-1935 was done

--- src/main/java/com/leetcode/dvd/NumberOfWordsCanType.java
+++ src/main/java/com/leetcode/dvd/NumberOfWordsCanType.java
@@ -1,4 +1,29 @@
 package com.leetcode.dvd;
 
 public class NumberOfWordsCanType {
+
+    public static void main(String[] args) {
+        String text = "abc de";
+        String brokenLetters = "";
+        System.out.println(canBeTypedWords(text,brokenLetters));
+    }
+
+    public static int canBeTypedWords(String text, String brokenLetters) {
+        String[] words = text.split(" ");
+        String[] letters = brokenLetters.split("");
+
+        int numberOfWords = words.length;
+        for (String word : words) {
+            int count = 0;
+            for (String letter : letters) {
+                if (!letter.isBlank() && word.contains(letter)){
+                    count++;
+                }
+            }
+            if (count>0){
+                numberOfWords--;
+            }
+        }
+        return numberOfWords;
+    }
 }
