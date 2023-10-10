b9e99582f286e4d2bb6edc456d75c9ce63f572d2
VishwaDinath <vishwadinath97@gmail.com>
2023-10-10 13:38:01 +0530

    Solved for a certain content

--- src/main/java/com/leetcode/dvd/RomanToInteger.java
+++ src/main/java/com/leetcode/dvd/RomanToInteger.java
@@ -1,5 +1,7 @@
 package com.leetcode.dvd;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RomanToInteger {
@@ -11,14 +13,35 @@ public class RomanToInteger {
     }
 
     public static int romanToInt(String s) {
+        ArrayList<String> special = new ArrayList<>();
+        special.add("IV");
+        special.add("IX");
+        special.add("XL");
+        special.add("XC");
+        special.add("CD");
+        special.add("CM");
+
+        int[] specialValues = {4,9,40,90,400,900};
+
         int number = 0;
-        for (int i = 0; i < s.length(); i++) {
-
-            for (int j = 0; j < s.length()-1; j++) {
-                String pair = s.substring(j,j+2);
+        String rest = "";
+        for (int j = 0; j < s.length()-1; j++) {
+            String pair = s.substring(j,j+2);
+            if (special.contains(pair)){
+                number += specialValues[special.indexOf(pair)];
+                j++;
+            }else{
+                if (j==s.length()-2){
+                    rest += pair.substring(0,2);
+                }else{
+                    rest += pair.substring(0,1);
+                }
             }
+        }
+        System.out.println(rest);
+        for (int i = 0; i < rest.length(); i++) {
 
-            String temp = s.substring(i, i + 1);
+            String temp = rest.substring(i, i + 1);
 
             switch (temp){
                 case "I":
@@ -39,7 +62,7 @@ public class RomanToInteger {
                 case "D":
                     number += 500;
                     break;
-                case "1000":
+                case "M":
                     number += 1000;
                     break;
             }
